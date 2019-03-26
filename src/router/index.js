de38cb2bed44b837f3c02dd85c36a081d1477e80
xialeiww <735087130@qq.com>
2019-03-26 15:35:36 +0800

    第二次测试

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import login from '@/views/login/login'
 
 Vue.use(Router)
 
@@ -13,7 +14,7 @@ export default new Router({
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/views/login/login')
+      component: login
     }
   ]
 })
