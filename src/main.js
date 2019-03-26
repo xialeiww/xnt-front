2d906ac263c27441d9b4c332eb33101bf80d8939
xialeiww <735087130@qq.com>
2019-03-26 16:06:53 +0800

    登陆模块开发

--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import VCharts from 'v-charts'
 
+import '@/styles/index.scss' // global css
+
+Vue.config.productionTip = false
+Vue.use(ElementUI, { locale })
+Vue.use(VCharts)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
