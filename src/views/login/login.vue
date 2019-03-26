707edca60d33c61b3bdd1c35f7879e0f4e9bb826
llllllzx <llllllllll_zx@163.com>
2019-03-26 15:44:07 +0800

    'test'

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="login">
         <form action="">
-            <label for="username">用户名：</label>
+            <label for="username">用户名：fuck ui</label>
             <input type="text" name="username" v-model="username">
             <label for="password">密码：</label>
             <input type="text" name="password" v-model="password">
